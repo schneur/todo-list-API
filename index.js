af97cb69f7a925ce1f225cfd5c497d41c5da385e
Schneur <schneuryiftach@gmail.com>
2021-07-05 15:03:51 -0400

    new tasks are updated in DOM

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 $(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
-    dataType: 'json',
-    success: function (response, textStatus) {
-      response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-      })
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  });
-
-
-var createTask = function () {
-  $.ajax({
-    type: 'POST',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-      task: {
-        content: $('#new-task-content').val()
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
       }
-    }),
-    success: function (response, textStatus) {
-      console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  });  
-}
-
-$('#create-task').on('submit', function (e) {
-  e.preventDefault();
-  createTask();
-});
-
-
+    });
+  }
+  
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
+  
+  getAndDisplayAllTasks();
+  
 });
