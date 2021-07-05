e718e5c6330555aae5b777a66f70867e8538606b
Schneur <schneuryiftach@gmail.com>
2021-07-05 16:03:52 -0400

    updated with ability to remove tasks and mark as complete using API

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
-        })
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+        });
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -43,5 +43,44 @@ $(document).ready(function(){
   });
   
   getAndDisplayAllTasks();
+
+
+  var deleteTask = function (id) {
+    $.ajax({
+   type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=111',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
   
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+   type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=111',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+       markTaskComplete($(this).data('id'));
+     }
+   });
+
 });
