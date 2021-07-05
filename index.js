d0994b168075065044a835cc3d8a2a076aa4d460
Schneur <schneuryiftach@gmail.com>
2021-07-05 16:14:57 -0400

    updated with ability to mark tasks as complete or active

--- index.js
+++ index.js
@@ -77,10 +77,26 @@ $(document).ready(function(){
     });
   }
 
+  var markTaskActive = function (id) {
+    $.ajax({
+   type: 'PUT',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=111',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
   $(document).on('change', '.mark-complete', function () {
     if (this.checked) {
        markTaskComplete($(this).data('id'));
-     }
+     } else {
+      markTaskActive($(this).data('id'));
+    }
    });
 
 });
