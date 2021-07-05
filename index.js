36947a6521a205f3735213d06f99e0e43dc6db6f
Schneur <schneuryiftach@gmail.com>
2021-07-05 14:47:16 -0400

    updated

---
+++ index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function (task) {
+        $('#todo-list').append('<p>' + task.content + '</p>');
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+});
+
+$.ajax({
+  type: 'POST',
+  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
+  contentType: 'application/json',
+  dataType: 'json',
+  data: JSON.stringify({
+    task: {
+      content: 'Do something fun'
+    }
+  }),
+  success: function (response, textStatus) {
+    console.log(response);
+  },
+  error: function (request, textStatus, errorMessage) {
+    console.log(errorMessage);
+  }
+});
