4a1ba8ac49da0572587533a86ab3207089614127
Schneur <schneuryiftach@gmail.com>
2021-07-05 14:57:03 -0400

    can now create task with my API

--- index.js
+++ index.js
@@ -12,22 +12,32 @@ $(document).ready(function(){
       console.log(errorMessage);
     }
   });
+
+
+var createTask = function () {
+  $.ajax({
+    type: 'POST',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: $('#new-task-content').val()
+      }
+    }),
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });  
+}
+
+$('#create-task').on('submit', function (e) {
+  e.preventDefault();
+  createTask();
 });
 
-$.ajax({
-  type: 'POST',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=111',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: JSON.stringify({
-    task: {
-      content: 'Do something fun'
-    }
-  }),
-  success: function (response, textStatus) {
-    console.log(response);
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
+
 });
